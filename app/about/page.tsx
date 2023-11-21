90580212cbc188b388516e3f7b2ea9e15e7444b6
devmianharoon <devmianharoon@gmail.com>
2023-11-21 17:52:37 +0500

    final

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import about from "../../public/assest/about.png";
 import NAVBAR from "../(componant)/navbar/page";
-import HEADER from "../(componant)/header/page";
 import Link from "next/link";
 
 export default function ABOUT() {
@@ -9,35 +8,29 @@ export default function ABOUT() {
     return (
         <div>
             <section className="Sub-header">
-        <nav>
-            <Link href="/" className="logo">Xplore
-                <i className="fab fa-staylinked"></i>kill
-            </Link>
-            <div className="nav-links" id="navLinks">
-                {/* <!-- reposnive bar open and close --> */}
-                {/* <i className="fa fa-times" onclick={hideMenu}></i> */}
-                <ul>
-                    <li><Link href="/index">Home</Link></li>
-                    <li><Link href="/course">Course</Link></li>
-                    <li><Link href="/blog">Blog</Link></li>
-                    <li><Link href="/about">About</Link></li>
-                    <li><Link href="/contact">Contact</Link></li>
-                </ul>
-            </div>
-            {/* <i className="fa fa-bars" onClick={showMenu}></i> */}
-            {/* <!-- reposnive bar open and close --> */}
-        </nav>
-        <h1>About Us</h1>
-    </section>
+                <nav>
+                <Link href="/" className="logo">Xplore<i className="fab fa-staylinked"></i>kill</Link>
+                    <div className="nav-links" id="navLinks">
+                        <ul>
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/course">Course</Link></li>
+                            <li><Link href="/blog">Blog</Link></li>
+                            <li><Link href="/about">About</Link></li>
+                            <li><Link href="/contact">Contact</Link></li>
+                        </ul>
+                    </div>
+                </nav>
+                <h1>About Us</h1>
+            </section>
 
             {/* <!-- About Us Section Start --> */}
 
             <section className="about-us">
                 <div className="row">
                     <div className="about-col">
-                        <h1>We are the world's largest University</h1>
+                        <h1>We are the worlds largest University</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
-                        <a href="#" className="hero_btn btn">EXPLORE NOW</a>
+                        <Link href="/" className="hero_btn btn">EXPLORE NOW</Link>
                     </div>
                     <div className="about-col">
                         <Image src={about} alt="" />
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap"/>
+    {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap"/> */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
